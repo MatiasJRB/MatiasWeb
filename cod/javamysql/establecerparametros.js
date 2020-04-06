822021923f0395af4b35d1e1dae8c27f098d6081
MatiasJRB <mjrcal95@hotmail.com.ar>
2020-04-05 23:23:44 -0300

    asd

---
+++ cod/javamysql/establecerparametros.js
@@ -0,0 +1,9 @@
+﻿public void establecerParametros()
+{
+    driver = "com.ibm.db2.jcc.DB2Driver";
+	servidor = "181.228.144.21:51552";
+	baseDatos = "mjRios";
+	usuario = "userExterno";
+	clave = "passExterno";
+	urlConexion = "jdbc:mysql://" + servidor + "/" + baseDatos;
+}
