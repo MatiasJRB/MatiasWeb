40d046ae50cca85544f7f43570d50a36fc3ef9e3
MatiasJRB <mjrcal95@hotmail.com.ar>
2021-02-23 16:59:00 -0300

    locuras nueva web

--- cod/javamysql/establecerparametros.js
+++
@@ -1,9 +0,0 @@
-﻿public void establecerParametros()
-{
-    driver = "com.ibm.db2.jcc.DB2Driver";
-	servidor = "181.228.144.21:51552";
-	baseDatos = "mjRios";
-	usuario = "userExterno";
-	clave = "passExterno";
-	urlConexion = "jdbc:mysql://" + servidor + "/" + baseDatos;
-}
